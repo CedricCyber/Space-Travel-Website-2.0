9469183d3bdb69f13f2c215c28c6347b48685f7c
Cedric Hill <104404794+CedricCyber@users.noreply.github.com>
2022-11-29 01:19:16 -0600

    added index.js & explore button animation

---
+++ index.js
@@ -0,0 +1,12 @@
+// Navbar active underline
+// Dom Elements
+const navHome = document.getElementsByClassName("home")[0];
+const navDestination = document.getElementsByClassName("destination")[0];
+const navCrew = document.getElementsByClassName("crew")[0];
+const navTechnology = document.getElementsByClassName("technology")[0];
+const navItems = document.getElementsByClassName("home-page-nav-item");
+//
+console.log(navItems);
+console.log(navItems.classList);
+
+// Todo: Find a way to add an active status to your selected nav bar item
